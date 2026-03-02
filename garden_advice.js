e9293377b84204883506e29deeb08ba91af3821c
RioGra <gmriordan@gmail.com>
2026-03-02 15:49:20 +0000

    Add detailed comments to code (closes #2)

--- garden_advice.js
+++ garden_advice.js
@@ -1,38 +1,43 @@
-// Refactor the code into functions
-// Function to get user input to allow advice
-// based on user input for season  and plant type.
+// Garden Advice App
+// Provides gardening advice based on the season and plant type
 
-// Function to get season advice
+// Function gives advice based on the season
 function getSeasonAdvice(season) {
+	// Checks if its summer
 	if (season === "summer") {
 		return "Water your plants regularly and provide some shade.\n";
+		//Checks if its winter
 	} else if (season === "winter") {
 		return "Protect your plants from frost with covers.\n";
+		// If season is not summer or winter, it will return this message
 	} else {
 		return "No advice for this season.\n";
 	}
 }
 
-//Function to get plant type advice
+//Function gives advice based on plant type
 function getPlantTypeAdvice(plantType) {
+	// Check if its a flower
 	if (plantType === "flower") {
 		return "Use fertilizer to encourage blooms.";
+		// Check if its a vegetable
 	} else if (plantType === "vegetable") {
 		return "Keep an eye out for pests!";
+		// If plant type is not flower or vegetable, it will return this message
 	} else {
 		return "No advice for this type of plant.";
 	}
 }
 
-// Get user input
+//Ask user what season it is
 let season = prompt("What season is it? (summer, winter, spring, fall)?");
+// Ask the user what type of plant they have
 let plantType = prompt(
 	"What plant type do you have?(flower, vegetable, etc.)?",
 );
 
-// Get advice from functions
+// Combine the advice from both functions
 let advice = getSeasonAdvice(season) + getPlantTypeAdvice(plantType);
 
 // Display advice
 console.log(advice);
-
